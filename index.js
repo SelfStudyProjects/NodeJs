bcec25ebc6f3ac17879b0213e762c016bfd43a8c
najasinis <tttjjjyyy1234@daum.net>
2025-02-06 10:58:19 +0900

    update file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+mongodb+srv://jerry:tj2364458@cluster0.g2sqe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
 app.get('/', (req, res) => {
   res.send('Hello World! 오호라디야. :D')
 })
