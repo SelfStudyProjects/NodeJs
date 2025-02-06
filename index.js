2666059d67af84faf2d199aa3adfe92498d78a19
najasinis <tttjjjyyy1234@daum.net>
2025-02-06 12:00:11 +0900

    update file

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-mongodb+srv://jerry:tj2364458@cluster0.g2sqe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://jerry:tj2364458@cluster0.g2sqe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World! 오호라디야. :D')
