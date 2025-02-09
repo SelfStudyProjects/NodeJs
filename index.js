c9bfca28d3d62d53bc75e59b07bd8fb3c20cc330
najasinis <tttjjjyyy1234@daum.net>
2025-02-10 08:03:53 +0900

    update file

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const { User } = require('./models/Users')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jerry:tj2364458@cluster0.g2sqe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err))
+mongoose.set('strictQuery', false)  // 경고 메시지 제거
 
+// 연결 옵션 설정
+const connectOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect('mongodb+srv://jerry:tj2364458@cluster0.g2sqe.mongodb.net/project_0?retryWrites=true&w=majority&appName=Cluster0', connectOptions)
+    .then(() => console.log('MongoDB Connected Successfully...'))
+    .catch(err => console.log('MongoDB Connection Error:', err))
+    
 app.get('/', (req, res) => {
-  res.send('Hello World! 오호라디야. :D')
+    res.send('Hello World! 오호라디야. :D')
+        })
+
+
+app.post('/register', (req, res) => {
+    //회원 가입할 때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터베이스에 넣어줘야 한다.
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
